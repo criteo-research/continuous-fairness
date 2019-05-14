3af3e413538ffc4f79bfa7fa831a667163acc5f2
Jeremie Mary <j.mary@c02vx11xhv2r.criteois.lan>
2019-05-14 18:00:14 +0200

    Adult example added

--- examples/data_loading/datasets.py
+++ examples/data_loading/datasets.py
@@ -1,9 +1,11 @@
 import os
 import urllib
 import os.path
-
 import numpy as np
-from pandas import read_csv
+import pandas as pd
+import sklearn.preprocessing as preprocessing
+from sklearn.preprocessing import StandardScaler
+from collections import namedtuple
 
 dirname = os.path.dirname(__file__)
 
@@ -14,6 +16,8 @@ def read_dataset(name, label=None, sensitive_attribute=None, fold=None):
         z_name = sensitive_attribute if sensitive_attribute is not None else 'racepctblack'
         fold_id = fold if fold is not None else 1
         return read_crimes(label=y_name, sensitive_attribute=z_name, fold=fold_id)
+    if name=='adult':
+        return load_adult()
     else:
         raise NotImplemented('Dataset {} does not exists'.format(name))
 
@@ -34,7 +38,7 @@ def read_crimes(label='ViolentCrimesPerPop', sensitive_attribute='racepctblack',
                 names.append(line.split(' ')[1])
 
     # load data
-    data = read_csv('communities.data', names=names, na_values=['?'])
+    data = pd.read_csv('communities.data', names=names, na_values=['?'])
 
     to_drop = ['state', 'county', 'community', 'fold', 'communityname']
     data.fillna(0, inplace=True)
@@ -56,3 +60,104 @@ def read_crimes(label='ViolentCrimesPerPop', sensitive_attribute='racepctblack',
 
     x = np.array(data.values)
     return x[folds != fold], y[folds != fold], z[folds != fold], x[folds == fold], y[folds == fold], z[folds == fold]
+
+
+
+
+
+
+#This function is a minor modification from https://github.com/jmikko/fair_ERM
+def load_adult(nTrain=None, scaler=True, shuffle=False):
+    if shuffle:
+        print('Warning: I wont shuffle because adult has fixed test set')
+    '''
+    :param smaller: selecting this flag it is possible to generate a smaller version of the training and test sets.
+    :param scaler: if True it applies a StandardScaler() (from sklearn.preprocessing) to the data.
+    :return: train and test data.
+
+    Features of the Adult dataset:
+    0. age: continuous.
+    1. workclass: Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked.
+    2. fnlwgt: continuous.
+    3. education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th,
+    Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool.
+    4. education-num: continuous.
+    5. marital-status: Married-civ-spouse, Divorced, Never-married, Separated, Widowed,
+    Married-spouse-absent, Married-AF-spouse.
+    6. occupation: Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty,
+    Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv,
+    Protective-serv, Armed-Forces.
+    7. relationship: Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried.
+    8. race: White, Asian-Pac-Islander, Amer-Indian-Eskimo, Other, Black.
+    9. sex: Female, Male.
+    10. capital-gain: continuous.
+    11. capital-loss: continuous.
+    12. hours-per-week: continuous.
+    13. native-country: United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc),
+    India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica, Vietnam, Mexico,
+    Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti, Columbia, Hungary, Guatemala,
+    Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador, Trinadad&Tobago, Peru, Hong, Holand-Netherlands.
+    (14. label: <=50K, >50K)
+    '''
+    if not os.path.isfile('adult.data'):
+        urllib.request.urlretrieve(
+            "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data", "adult.data")
+        urllib.request.urlretrieve(
+            "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test", "adult.test")
+    data = pd.read_csv(
+        "adult.data",
+        names=[
+            "Age", "workclass", "fnlwgt", "education", "education-num", "marital-status",
+            "occupation", "relationship", "race", "gender", "capital gain", "capital loss",
+            "hours per week", "native-country", "income"]
+            )
+    len_train = len(data.values[:, -1])
+    data_test = pd.read_csv(
+        "adult.test",
+        names=[
+            "Age", "workclass", "fnlwgt", "education", "education-num", "marital-status",
+            "occupation", "relationship", "race", "gender", "capital gain", "capital loss",
+            "hours per week", "native-country", "income"],
+        skiprows=1, header=None
+    )
+    data = pd.concat([data, data_test])
+    # Considering the relative low portion of missing data, we discard rows with missing data
+    domanda = data["workclass"][4].values[1]
+    data = data[data["workclass"] != domanda]
+    data = data[data["occupation"] != domanda]
+    data = data[data["native-country"] != domanda]
+    # Here we apply discretisation on column marital_status
+    data.replace(['Divorced', 'Married-AF-spouse',
+                  'Married-civ-spouse', 'Married-spouse-absent',
+                  'Never-married', 'Separated', 'Widowed'],
+                 ['not married', 'married', 'married', 'married',
+                  'not married', 'not married', 'not married'], inplace=True)
+    # categorical fields
+    category_col = ['workclass', 'race', 'education', 'marital-status', 'occupation',
+                    'relationship', 'gender', 'native-country', 'income']
+    for col in category_col:
+        b, c = np.unique(data[col], return_inverse=True)
+        data[col] = c
+    datamat = data.values
+    #Care there is a final dot in the class only in test set which creates 4 different classes
+    target = np.array([-1.0 if (val == 0 or val==1) else 1.0 for val in np.array(datamat)[:, -1]])
+    datamat = datamat[:, :-1]
+    if scaler:
+        scaler = StandardScaler()
+        scaler.fit(datamat)
+        datamat = scaler.transform(datamat)
+    if nTrain is None:
+        nTrain = len_train
+    data = namedtuple('_', 'data, target')(datamat[:nTrain, :], target[:nTrain])
+    data_test = namedtuple('_', 'data, target')(datamat[len_train:, :], target[len_train:])
+
+    encoded_data = pd.DataFrame(data.data)
+    encoded_data['Target'] = (data.target+1)/2
+    to_protect = 1. * (data.data[:,9]!=data.data[:,9][0])
+
+    encoded_data_test = pd.DataFrame(data_test.data)
+    encoded_data_test['Target'] = (data_test.target+1)/2
+    to_protect_test = 1. * (data_test.data[:,9]!=data_test.data[:,9][0])
+
+    #Variable to protect (9:Sex) is removed from dataset
+    return encoded_data.drop(columns=9), to_protect, encoded_data_test.drop(columns=9), to_protect_test
--- facl/independence/hgr.py
+++ facl/independence/hgr.py
@@ -10,6 +10,7 @@ def joint_2(X, Y, density, damping=1e-10):
     joint_density = density(data)
 
     nbins = int(min(50, 5. / joint_density.std))
+    #nbins = np.sqrt( Y.size/5 )
     x_centers = torch.linspace(-2.5, 2.5, nbins)
     y_centers = torch.linspace(-2.5, 2.5, nbins)
 
@@ -20,17 +21,15 @@ def joint_2(X, Y, density, damping=1e-10):
     return h2d
 
 
-def hgr(X, Y, density):
-    damping = 1e-10
+def hgr(X, Y, density, damping = 1e-10):
     h2d = joint_2(X, Y, density, damping=damping)
     marginal_x = h2d.sum(dim=1).unsqueeze(1)
     marginal_y = h2d.sum(dim=0).unsqueeze(0)
     Q = h2d / (torch.sqrt(marginal_x) * torch.sqrt(marginal_y))
-    return np.array(torch.svd(Q)[1][1])
+    return torch.svd(Q)[1][1]
 
 
-def chi_2(X, Y, density):
-    damping = 0
+def chi_2(X, Y, density, damping = 1e-10):
     h2d = joint_2(X, Y, density, damping=damping)
     marginal_x = h2d.sum(dim=1).unsqueeze(1)
     marginal_y = h2d.sum(dim=0).unsqueeze(0)
@@ -75,3 +74,42 @@ def chi_2_cond(X, Y, Z, density):
     marginal_yz = h3d.sum(dim=0).unsqueeze(0)
     Q = h3d / (torch.sqrt(marginal_xz) * torch.sqrt(marginal_yz))
     return ((Q ** 2).sum(dim=[0, 1]) - 1.)
+
+
+def K(x):
+    return torch.Tensor([1./np.sqrt(2*np.pi)]).float() * torch.exp(-x**2/2)
+
+def kde1D(x, centers, bandwidth=None):
+    if bandwidth is None:
+        n = x.size(0)
+        d = 1 #- 1
+        bandwidth = (n * (d + 2) / 4.)**(-1. / (d + 4))
+        bandwidth = bandwidth ** 2
+    #TODO : better handling of borders using the missing part of the kernel
+    return K((centers.reshape((-1,1)) - x)/bandwidth).mean(dim=1)/bandwidth
+
+
+def binary_renyi2_differentiable(X,Y, reg=1e-4, nbins=0, standardize = True):
+    #TODO: check that Y has exactly only two values and that X Y are of same length
+    cy = Y.sum().item()
+    if cy <= 1 or cy >= Y.size(0)-1:
+        return torch.tensor(0.)
+    if standardize:
+        X = (X - X.mean())/X.std()
+    if nbins== 0:
+        nbins = int(np.sqrt(Y.size(0)))
+    xmin = X.min().item()
+    xmax = X.max().item()
+    if xmin == xmax :
+        return torch.Tensor(0.)
+    centers = torch.linspace(xmin, xmax, nbins)
+    delta = centers[1]-centers[0]
+    bw = 2*(xmax-xmin)/nbins
+    h0 = kde1D(X[Y==0], centers, bw) #* delta * Y.size(0)
+    h1 = kde1D(X[Y==1], centers, bw) #* delta * Y.size(0)
+    h2d = torch.stack((h0,h1),dim=1) + reg
+    marginal_x=h2d.sum(dim=1)
+    marginal_y=h2d.sum(dim=0)
+    Q = torch.diag(1/marginal_x)@(h2d**2)@torch.diag(1/marginal_y)
+    return (Q.sum()-1).clamp(reg,1)
+    
