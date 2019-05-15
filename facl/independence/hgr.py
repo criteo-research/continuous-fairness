e0ee2c7512842d384b97139752eb0d9db30e8c61
Jeremie Mary <j.mary@c02vx11xhv2r.criteois.lan>
2019-05-15 10:31:14 +0200

    unification of KDE and renyi2

--- facl/independence/hgr.py
+++ facl/independence/hgr.py
@@ -76,40 +76,4 @@ def chi_2_cond(X, Y, Z, density):
     return ((Q ** 2).sum(dim=[0, 1]) - 1.)
 
 
-def K(x):
-    return torch.Tensor([1./np.sqrt(2*np.pi)]).float() * torch.exp(-x**2/2)
-
-def kde1D(x, centers, bandwidth=None):
-    if bandwidth is None:
-        n = x.size(0)
-        d = 1 #- 1
-        bandwidth = (n * (d + 2) / 4.)**(-1. / (d + 4))
-        bandwidth = bandwidth ** 2
-    #TODO : better handling of borders using the missing part of the kernel
-    return K((centers.reshape((-1,1)) - x)/bandwidth).mean(dim=1)/bandwidth
-
-
-def binary_renyi2_differentiable(X,Y, reg=1e-4, nbins=0, standardize = True):
-    #TODO: check that Y has exactly only two values and that X Y are of same length
-    cy = Y.sum().item()
-    if cy <= 1 or cy >= Y.size(0)-1:
-        return torch.tensor(0.)
-    if standardize:
-        X = (X - X.mean())/X.std()
-    if nbins== 0:
-        nbins = int(np.sqrt(Y.size(0)))
-    xmin = X.min().item()
-    xmax = X.max().item()
-    if xmin == xmax :
-        return torch.Tensor(0.)
-    centers = torch.linspace(xmin, xmax, nbins)
-    delta = centers[1]-centers[0]
-    bw = 2*(xmax-xmin)/nbins
-    h0 = kde1D(X[Y==0], centers, bw) #* delta * Y.size(0)
-    h1 = kde1D(X[Y==1], centers, bw) #* delta * Y.size(0)
-    h2d = torch.stack((h0,h1),dim=1) + reg
-    marginal_x=h2d.sum(dim=1)
-    marginal_y=h2d.sum(dim=0)
-    Q = torch.diag(1/marginal_x)@(h2d**2)@torch.diag(1/marginal_y)
-    return (Q.sum()-1).clamp(reg,1)
     
